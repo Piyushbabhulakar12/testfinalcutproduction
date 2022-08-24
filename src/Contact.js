d16d6941b1997d933791c855a373f2b302f3210d
Piyush Babhulakar <65109923+Piyushbabhulakar12@users.noreply.github.com>
2022-08-24 23:24:12 +0530

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactHero from './Contact/ContactHero'
+
+function Contact() {
+  return (
+    <div>
+       <ContactHero />
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/Home/Footer.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+
+function Footer() {
+    return (
+      <div>
+        
+             
+        <div class=" bg_padding_rem mt-5">
+              
+              <div class="container">
+                 <div class="row">
+                    <div class="col-md-3">
+                               <h3 class="footer_logo">Final Cut</h3>    
+                    </div>
+                    <div class="col-md-3">
+                        
+                        <p class="f_para_b">About</p>
+                        <p class="f_para">Photo Editor</p>
+                        <p class="f_para">Video Editor</p>
+                        <p class="f_para">Photo Editor</p>
+                        <p class="f_para">Photo Editor</p>
+                        <p class="f_para">Video Editor</p>
+                      
+
+
+                    </div>
+                    <div class="col-md-3">
+                        
+                        <p class="f_para_b">Support</p>
+                        <p class="f_para">Contact Us</p>
+                        <p class="f_para">Video Editor</p>
+                        <p class="f_para">Photo Editor</p>
+                       
+                    </div>
+                    <div class="col-md-3">
+                      
+                        <p class="f_para_b">Social</p>
+                        <p class="f_para">Instagram</p>
+                        <p class="f_para">Facebook</p>
+                        <p class="f_para">Twitter</p>
+                        <p class="f_para">Linkdin</p>
+                        <p class="f_para">Youtube</p>
+
+                    </div>
+                 </div>
+              </div>
+
+              <div class="container mt-4">
+                 <div class="row">
+                    <div class="col-md-3">
+                                 
+                    </div>
+                    <div class="col-md-3">
+                        
+                        <p class="f_para_b">For any Questions</p>
+                        <p class="f_para">finalcut@gmail.com</p>
+                        <p class="f_para">Telegram</p>
+                        <p class="f_para">What's App</p>
+                       
+                      
+
+
+                    </div>
+                    <div class="col-md-3">
+                        
+                        <p class="f_para_b">Contact Us</p>
+                        <p class="f_para">+91 445-548-54</p>
+                        <p class="f_para">finalcut@gmail.com</p>
+                    
+                       
+                    </div>
+                    <div class="col-md-3">
+                      
+                        <p class="f_para_b">Get Direction</p>
+                        <p class="f_para">XYZ , Demo Adderess <br/> Lorem Ipsum is simply <br/> dummy text of the printing</p>
+                        
+                    </div>
+                    
+                 </div>
+              </div>
+              <div class="container mt-3">
+                  <div class="row">
+                  <div class="col-12">
+                    <p class="f_para"><center>&copy; FinalCut Production All Rights Resvered</center></p>
+                    </div>
+                  </div>
+              </div>
+
+        </div>
+         
+        
+      </div>
+    )
+
+}
+
+  export default Footer;
